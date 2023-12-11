ebc00cf12debf0eb40f5709f03efc053adc304de
rzak23 <reza.akbar2396@gmail.com>
2023-12-11 10:50:25 +0800

    tambah keterangan waktu

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var moment = require('moment');
 
+setInterval(() => {
+    only_up();
+}, 1000);
+
 var tgl = document.getElementById('tgl');
 var blyear = document.getElementById('bl-year');
 var hari = document.getElementById('hari');
@@ -9,3 +13,11 @@ hari.innerHTML = moment().locale('id').format('dddd');
 var bulan = moment().locale('id').format('MMMM');
 var tahun = moment().year();
 blyear.innerHTML = `${bulan} ${tahun}`;
+
+function only_up(){
+    let jam = moment().format('HH');
+    let menit = moment().format('mm');
+    let detik = moment().format('ss');
+
+    document.getElementById('time-clock').innerHTML = `${jam} : ${menit} : ${detik}`;
+}
